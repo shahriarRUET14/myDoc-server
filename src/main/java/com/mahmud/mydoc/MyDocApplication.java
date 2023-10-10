54c7140f4f2d15e06604b19f3e833dc4f54f6f88
shahriarRUET14 <shahriar.ruet14@gmail.com>
2023-10-10 23:41:49 +0600

    init

---
+++ src/main/java/com/mahmud/mydoc/MyDocApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.mydoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDocApplication.class, args);
+    }
+
+}
