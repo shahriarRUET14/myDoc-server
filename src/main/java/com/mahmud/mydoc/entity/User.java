cdeea9d53cacc9f166714a87a3bef00c31606e17
shahriarRUET14 <shahriar.ruet14@gmail.com>
2023-10-14 22:59:35 +0600

    user signup signin and Security config basic implemented

--- src/main/java/com/mahmud/mydoc/entity/User.java
+++ src/main/java/com/mahmud/mydoc/entity/User.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -17,14 +16,14 @@ import java.util.List;
 @Table(name = "users")
 public class User {
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String name;
-
+    @Column(nullable = false)
+    private String username;
     @Column(nullable = false, unique = true)
     private String email;
 
@@ -32,9 +31,8 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
-            inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
-    private List<Role> roles = new ArrayList<>();
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 }
--- src/main/java/com/mahmud/mydoc/repository/UserRepository.java
+++ src/main/java/com/mahmud/mydoc/repository/UserRepository.java
@@ -3,7 +3,12 @@ package com.mahmud.mydoc.repository;
 import com.mahmud.mydoc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
-    User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
